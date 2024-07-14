162991e40a8f8f91d5f62da9613ec63a112efa7e
Jonatas Flores <jonatasfloresvianadossantos@gmail.com>
2024-07-13 21:45:57 -0300

    Feat: Envolvido o Container principal em um widget para visualização com rolagem, e adicionado tipos de teclado específicos para os campos dificuldade e imagem keybordType

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -20,86 +20,91 @@ class _FormScreenState extends State<FormScreen> {
         titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
       ),
       body: Center(
-        child: Container(
-          height: 650,
-          width: 375,
-          decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                width: 3,
-              )),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                    controller: nameController,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Nome',
-                      fillColor: Colors.white70,
-                      filled: true,
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                    controller: difficultyController,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Dificuldade',
-                      fillColor: Colors.white70,
-                      filled: true,
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                    onChanged: (text) {
-                      setState(() {});
-                    },
-                    controller: imageController,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Imagem',
-                      fillColor: Colors.white70,
-                      filled: true,
-                    )),
-              ),
-              Container(
-                height: 100,
-                width: 72,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(
-                    10,
-                  ),
-                  border: Border.all(width: 2, color: Colors.blue),
+        child: SingleChildScrollView(
+          child: Container(
+            height: 650,
+            width: 375,
+            decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  width: 3,
+                )),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                      controller: nameController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Nome',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      )),
                 ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    imageController.text,
-                    errorBuilder: (BuildContext context, Object exception,
-                        StackTrace? stackTrace) {
-                      return Container();
-                    },
-                    fit: BoxFit.cover,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: difficultyController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Dificuldade',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      )),
+                ),
+                
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                      onChanged: (text) {
+                        setState(() {});
+                      },
+                      keyboardType: TextInputType.url,
+                      controller: imageController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Imagem',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      )),
+                ),
+                Container(
+                  height: 100,
+                  width: 72,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ),
+                    border: Border.all(width: 2, color: Colors.blue),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      imageController.text,
+                      errorBuilder: (BuildContext context, Object exception,
+                          StackTrace? stackTrace) {
+                        return Container();
+                      },
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print('Sucesso Nova Tarefa Criada');
-                  },
-                  child: Text('Adicionar'))
-            ],
+                ElevatedButton(
+                    onPressed: () {
+                      print('Sucesso Nova Tarefa Criada');
+                    },
+                    child: Text('Adicionar'))
+              ],
+            ),
           ),
         ),
       ),
