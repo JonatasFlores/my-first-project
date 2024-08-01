b1f3e916b01592095d389f54eb02b23c7411e84d
Jonatas Flores <jonatasfloresvianadossantos@gmail.com>
2024-08-01 01:30:45 -0300

    Feat: Criado pasta data e 1 arquivo inheritedWidget para armanezar a lista de tarefas

---
+++ lib/data/task_inherited.dart
@@ -0,0 +1,36 @@
+import 'package:alura_flutter_curso_1/components/tasks.dart';
+import 'package:flutter/material.dart';
+
+class TaskInherited extends InheritedWidget {
+  TaskInherited({
+    Key? key,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final List<Tasks> taskList = [
+    Tasks('Estudar Flutter', 'assets/images/flutter.png', 3),
+    Tasks('Andar de Bike', 'assets/images/bike.webp', 2),
+    Tasks('Ler 50 páginas', 'assets/images/ler.jpg', 1),
+    Tasks('Meditar', 'assets/images/meditar.jpeg', 4),
+    Tasks(
+      'Estudar Inglês',
+      'assets/images/aprender inglês.jpeg',
+      0,
+    ),
+  ];
+
+  void newTask(String name, String photo, int difficulty) {
+    taskList.add(Tasks(name, photo, difficulty));
+  }
+
+  static TaskInherited of(BuildContext context) {
+    final result = context.dependOnInheritedWidgetOfExactType<TaskInherited>();
+    assert(result != null, 'Widget not found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(TaskInherited old) {
+    return taskList != old.taskList;
+  }
+}
